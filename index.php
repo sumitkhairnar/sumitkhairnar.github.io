81c5fa5df9655987fe43a33947d32af0d555f12c
sumitkhairnar <sumitkhairnar@users.noreply.github.com>
2017-03-12 14:52:07 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html"; charset="utf-8"/>
+<title>registration form</title>
+</head>
+<body>
+<form id="formal" name="formal" method="post" action="index.php">
+<div align="center"
+<table width="426" height="235" border="1" align="center" cellpadding="8" cellspacing="0">
+  <caption>
+    <h2>registration form
+  </h2>
+    
+  </caption>
+  <tr>
+      <td width="165">name</td>
+      <td width="163">
+      <input type="text" name="name" id="textfield"/></td>
+    </tr>
+    <tr>
+      <td>email</td>
+      <td>
+      <input type="email" name="email" id="textfield2"/></td>
+    </tr>
+    <tr>
+      <td>username</td>
+      <td><input type="text" name="u_name" id="textfield3"/></td>
+    </tr>
+    <tr>
+      <td>password</td>
+      <td><input type="password" name="password" id="textfield4"/></td>
+    </tr>
+    <tr>
+      <td colspan="2"><input type="submit" name="submit" id="button" value="Submit" /></td>
+    
+	</tr>
+  </tbody>
+</table>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
